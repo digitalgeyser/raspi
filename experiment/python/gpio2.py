2d01ce0feaadbd3aa3cbaa15060f3a75deea8499
Timotej Ecimovic <timotej@digitalgeyser.com>
2015-12-27 19:01:46 +0000

    Minor cleanup.

--- experiment/python/gpio2.py
+++ experiment/python/gpio2.py
@@ -12,22 +12,27 @@ c.blueOff()
 
 time.sleep(delay)
 
+diff=1
 button=1
 consecutive = 0
 while consecutive < 400:
   button = GPIO.input(24)
   if button == 0:
-    consecutive += 1
+    consecutive += diff
     c.blueOn()
   else:
     consecutive = 0
     c.blueOff()
 
+  # If getting closer, we blink with higher frequency.
+  if ( consecutive > 200 ):
+    diff = 2
+
   if (consecutive/10) % 2 == 0:
     c.yellowOn()
   else:
     c.yellowOff()
-   
+
   time.sleep(delay)
 
 
